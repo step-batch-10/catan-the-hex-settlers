edbe5f678c2880bf0c34aced58533521f66b56ed
Ajay Reddy <iamajayreddy18@gmail.com>
2025-04-17 14:50:06 +0530

    [#26] Ajay/Surendra Added a Player class and tests for it
    What changes have you made?
    
    Added a player class
    
    Why do we need this change?
    
    To handle player related operations

---
+++ src/models/player.ts
@@ -0,0 +1,103 @@
+interface Resources {
+  wood: number;
+  brick: number;
+  sheep: number;
+  wheat: number;
+  ore: number;
+}
+
+interface DevCards {
+  knight: number;
+  'road-building': number;
+  'year-of-plenty': number;
+  monopoly: number;
+}
+
+export class Player {
+  id: string;
+  name: string;
+  color: string;
+  resources: Resources;
+  roads: string[];
+  settlements: string[];
+  cities: string[];
+  devCards: DevCards;
+  hasLongestRoad: boolean;
+  hasLargestArmy: boolean;
+  victoryPoints: number;
+
+  constructor(id: string, name: string, color: string) {
+    this.id = id;
+    this.name = name;
+    this.color = color;
+
+    this.resources = {
+      wood: 0,
+      brick: 0,
+      sheep: 0,
+      wheat: 0,
+      ore: 0
+    };
+
+    this.roads = [];
+    this.settlements = [];
+    this.cities = [];
+
+    this.devCards = {
+      knight: 0,
+      'road-building': 0,
+      'year-of-plenty': 0,
+      monopoly: 0
+    };
+
+    this.hasLongestRoad = false;
+    this.hasLargestArmy = false;
+    this.victoryPoints = 0;
+  }
+
+  hasWon(): boolean {
+    return this.victoryPoints >= 10;
+  }
+
+  getPlayerData(): {
+    id: string;
+    name: string;
+    color: string;
+    resources: Resources;
+    roads: string[];
+    settlements: string[];
+    cities: string[];
+    devCards: DevCards;
+    hasLargestArmy: boolean;
+    hasLongestRoad: boolean;
+    victoryPoints: number;
+  } {
+    const {
+      id,
+      name,
+      color,
+      resources,
+      roads,
+      settlements,
+      cities,
+      devCards,
+      hasLargestArmy,
+      hasLongestRoad,
+      victoryPoints
+    } = this;
+
+    return {
+      id,
+      name,
+      color,
+      resources,
+      roads,
+      settlements,
+      cities,
+      devCards,
+      hasLargestArmy,
+      hasLongestRoad,
+      victoryPoints
+    };
+  }
+}
