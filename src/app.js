f9d6495edc3cecc313187ca563448b2eb0227d79
Shalu Jha <183721575+Shalujha0907@users.noreply.github.com>
2025-04-18 14:47:12 +0530

    [#30]|Fixes formating issue

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import { Hono } from 'hono';
-import { serveStatic } from 'hono/deno';
-import { logger } from 'hono/logger';
-import { serveGamePage, serveGameState } from './handlers/dynamicHandlers.ts';
+import { Hono } from "hono";
+import { serveStatic } from "hono/deno";
+import { logger } from "hono/logger";
+import { serveGamePage, serveGameState } from "./handlers/dynamicHandlers.ts";
 
 const inject = (game) => async (c, next) => {
-  c.set('game', game);
+  c.set("game", game);
   await next();
 };
 
@@ -12,8 +12,8 @@ const gameRoutes = (game) => {
   const gameApp = new Hono();
 
   gameApp.use(inject(game));
-  gameApp.get('/gameState', serveGameState);
-  gameApp.get('/:playerId', serveGamePage);
+  gameApp.get("/gameState", serveGameState);
+  gameApp.get("/:playerId", serveGamePage);
   return gameApp;
 };
 
@@ -21,10 +21,10 @@ export const createApp = (game) => {
   const app = new Hono();
 
   app.use(logger());
-  app.get('/ok', (c) => c.text('ok'));
+  app.get("/ok", (c) => c.text("ok"));
 
-  app.route('/game', gameRoutes(game));
+  app.route("/game", gameRoutes(game));
 
-  app.get('*', serveStatic({ root: './public' }));
+  app.get("*", serveStatic({ root: "./public" }));
   return app;
 };
