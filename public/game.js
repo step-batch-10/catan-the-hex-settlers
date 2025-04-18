f32f1cdf47030b205ea55034332c313c46062884
Adil Ali <106112362+Adil8081@users.noreply.github.com>
2025-04-18 19:26:31 +0530

    [#26] rendering map | Ajay/Surendra/Adil
    What changes have you made?
    
    added html to game.html for board and added script to game.js to render the map
    
    Why do we need this change?
    
    To show the map to the players

--- public/game.js
+++ public/game.js
@@ -1,3 +1,73 @@
+const noTokenImages = {
+  2: '/images/tokens/token-2.png',
+  3: '/images/tokens/token-3.png',
+  4: '/images/tokens/token-4.png',
+  5: '/images/tokens/token-5.png',
+  6: '/images/tokens/token-6.png',
+  8: '/images/tokens/token-8.png',
+  9: '/images/tokens/token-9.png',
+  10: '/images/tokens/token-10.png',
+  11: '/images/tokens/token-11.png',
+  12: '/images/tokens/token-12.png'
+};
+
+const terrainImages = {
+  forest: '/images/terrains/forest.png',
+  pasture: '/images/terrains/pasture.png',
+  mountains: '/images/terrains/mountains.png',
+  hills: '/images/terrains/hills.png',
+  fields: '/images/terrains/fields.png',
+  desert: '/images/terrains/desert.png'
+};
+
+function* coordinates() {
+  const ar = [3, 4, 5, 4, 3];
+  for (const [index, length] of Object.entries(ar)) {
+    for (let tileIndex = 0; tileIndex < length; tileIndex++) {
+      yield {
+        x: (Math.floor(length / 3) - (length % 3)) * 50 + 100 * tileIndex,
+        y: 86.5 * index
+      };
+    }
+  }
+}
+
+const generateTiles = (tiles) => {
+  const coordinate = coordinates();
+
+  return tiles
+    .map((tile) => {
+      const coord = coordinate.next().value;
+      console.log(tile.terrain, tile.terrainNumber)
+      const header = `<g
+    transform="translate(${coord.x},${coord.y})"
+    >`;
+      
+      const tilesImage = `<image
+    href="${terrainImages[tile.terrain]}"
+    x="10"
+    y="10"
+    width="90"
+    height="110"
+    transform="translate(-155,-237.5)"
+    />`
+      
+      const resourceNumber = `<image href="${noTokenImages[tile.terrainNumber]}"
+    x="0"
+    y="0"
+    width="36"
+    height="36"
+    transform="translate(-120,-190)">
+    </g>`;
+  
+      const template = `${header} ${tilesImage}`;
+      
+      return tile.terrainNumber === null ? template : template + resourceNumber;
+    })
+    .join('\n');
+};
+
+
 const appendText = (template, elementId, text) => {
   const element = template.getElementById(elementId);
   element.textContent += text;
@@ -89,6 +159,12 @@ const renderDice = (diceId, value) => {
   dice.classList.add('dice');
 }
 
+const renderBoard = (hexes) => {
+  const tilescontainer = document.querySelector("#tilesContainer");
+  const tiles = generateTiles(hexes);
+  tilescontainer.innerHTML = tiles;
+}
+
 const renderBothDice = (diceRoll) => {
   ['dice1', 'dice2'].forEach((diceId, i) => renderDice(diceId, diceRoll[i]))
 } 
@@ -96,6 +172,7 @@ const renderBothDice = (diceRoll) => {
 const renderElements = (gameState) => {
   renderPlayersData(gameState.players);
   renderBothDice([3, 3]);
+  renderBoard(gameState.board.hexes)
 }
 
 const main = async () => {
